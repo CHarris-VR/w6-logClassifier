ea5e0fbbb1aef2424226308e37e3be192dfdb2d5
CHarris-VR <charrisagent@gmail.com>
2026-01-07 11:31:12 -0500

    printed successful logins

--- security_log_classifier.py
+++ security_log_classifier.py
@@ -5,8 +5,21 @@ with open("logins.txt") as f:
 
 print(f"Loaded {len(lines)} login records.")
 
-# Stores data into a list
+# Stores data into lists
 for line in lines:
     parts = line.strip().split()
     # parts = [username, ip, result]
 
+
+# Added this for the loop below
+failed_logins = 0
+successful_logins = 0
+
+# if failure, add a failed logins counter + 1 else success counter + 1
+
+if parts[2] == "FAILURE":
+    failed_logins += 1
+else:
+    successful_logins += 1
+
+print("successful logins: ", successful_logins)
