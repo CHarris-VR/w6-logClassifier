2e487dccd4d26de0b1b6c4498c38a1ab75c65abf
CHarris-VR <charrisagent@gmail.com>
2026-01-07 13:21:26 -0500

    learned some things and made changes for some of the extra credit

--- security_log_classifier.py
+++ security_log_classifier.py
@@ -6,6 +6,7 @@ external = 0
 users = []
 ips = []
 results = []
+failed_by_user = {}
 
 with open("logins.txt") as f:
     lines = f.readlines()
@@ -28,8 +29,24 @@ for line in lines:
     # Count success vs failure
     if result == "FAILURE":
         failed_logins += 1
+
+    # Track Failures    
+        if username in failed_by_user:
+            failed_by_user[username] += 1
+        else: 
+            failed_by_user[username] = 1
+
     else:
         successful_logins += 1
 
-print("Interal:", internal, "External:", external)
+
+# Printing standard Data
+print("Internal:", internal, "External:", external)
 print("Success:", successful_logins, "Failure:", failed_logins)
+
+
+# Detect Brute Force after Loop
+for user, count in failed_by_user.items():
+    if count >= 3:
+        print("\nPossible Brute Force Attempt!")
+        print(f"  {user} has {count} failed login attempts")
